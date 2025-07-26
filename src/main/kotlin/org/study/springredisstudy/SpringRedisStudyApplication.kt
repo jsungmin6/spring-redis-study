d03c27978b2f98d67825bb92114afcea57c6d0bb
김태민 <taemin@gimtaemin-ui-MacBookPro.local>
2025-07-26 21:15:12 +0900

    first commit

---
+++ src/main/kotlin/org/study/springredisstudy/SpringRedisStudyApplication.kt
@@ -0,0 +1,11 @@
+package org.study.springredisstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringRedisStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringRedisStudyApplication>(*args)
+}
